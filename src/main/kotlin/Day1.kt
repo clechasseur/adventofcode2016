74b21fbaf2d7304f2a9dbc53955a4cc2825df077
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2020-05-28 20:53:11 -0400

    Day 1 + 2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,3 +1,56 @@
+import org.clechasseur.adventofcode2016.Direction
+import org.clechasseur.adventofcode2016.Pt
+import org.clechasseur.adventofcode2016.manhattan
+import org.clechasseur.adventofcode2016.move
+
 object Day1 {
-    fun part1() = 0
+    private const val input = "R1, L3, R5, R5, R5, L4, R5, R1, R2, L1, L1, R5, R1, L3, L5, L2, R4, L1, R4, R5, L3, R5, L1, R3, L5, R1, L2, R1, L5, L1, R1, R4, R1, L1, L3, R3, R5, L3, R4, L4, R5, L5, L1, L2, R4, R3, R3, L185, R3, R4, L5, L4, R48, R1, R2, L1, R1, L4, L4, R77, R5, L2, R192, R2, R5, L4, L5, L3, R2, L4, R1, L5, R5, R4, R1, R2, L3, R4, R4, L2, L4, L3, R5, R4, L2, L1, L3, R1, R5, R5, R2, L5, L2, L3, L4, R2, R1, L4, L1, R1, R5, R3, R3, R4, L1, L4, R1, L2, R3, L3, L2, L1, L2, L2, L1, L2, R3, R1, L4, R1, L1, L4, R1, L2, L5, R3, L5, L2, L2, L3, R1, L4, R1, R1, R2, L1, L4, L4, R2, R2, R2, R2, R5, R1, L1, L4, L5, R2, R4, L3, L5, R2, R3, L4, L1, R2, R3, R5, L2, L3, R3, R1, R3"
+
+    private val moves = input.split(", ").map { it.toMove() }
+
+    fun part1(): Int {
+        val me = Me()
+        moves.forEach { me.apply(it) }
+        return manhattan(me.pos, Pt.ZERO)
+    }
+
+    fun part2(): Int {
+        val me = Me()
+        val visited = mutableSetOf<Pt>()
+        for (move in moves) {
+            val pts = me.apply(move)
+            pts.forEach {
+                if (visited.contains(it)) {
+                    return manhattan(it, Pt.ZERO)
+                }
+                visited.add(it)
+            }
+        }
+        error("No location was visited twice")
+    }
+
+    private enum class TurnDirection {
+        L, R
+    }
+
+    private data class Move(val turn: TurnDirection, val distance: Int)
+
+    private fun String.toMove() = Move(TurnDirection.valueOf(this[0].toString()), substring(1).toInt())
+
+    private class Me {
+        var pos = Pt.ZERO
+        var heading = Direction.UP
+
+        fun apply(move: Move): List<Pt> {
+            heading = if (move.turn == TurnDirection.L) {
+                heading.left
+            } else {
+                heading.right
+            }
+            return (0 until move.distance).map { _ ->
+                pos = pos.move(heading)
+                pos
+            }
+        }
+    }
 }
--- src/test/kotlin/AdventOfCode2016.kt
+++ src/test/kotlin/AdventOfCode2016.kt
@@ -5,7 +5,24 @@ class AdventOfCode2016 {
     class Day1Puzzles {
         @Test
         fun `day 1, part 1`() {
-            assertEquals(0, Day1.part1())
+            assertEquals(298, Day1.part1())
+        }
+
+        @Test
+        fun `day 1, part 2`() {
+            assertEquals(158, Day1.part2())
+        }
+    }
+
+    class Day2Puzzles {
+        @Test
+        fun `day 2, part 1`() {
+            assertEquals("56855", Day2.part1())
+        }
+
+        @Test
+        fun `day 2, part 2`() {
+            assertEquals("B3C27", Day2.part2())
         }
     }
 }
